2913edf912327b139b2c547f044dcb2a415bfb4d
davidhjensen <dhjensen02@gmail.com>
2024-04-20 16:44:43 -0600

    basic hello_world app

--- forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Mockup
-
-class MockupForm(forms.ModelForm):
-    class Meta:
-        model = Mockup
-        fields = ['company_name', 'helmet_color', 'helmet_model', 'size', 'front_logo', 'back_logo', 'left_logo', 'right_logo']
